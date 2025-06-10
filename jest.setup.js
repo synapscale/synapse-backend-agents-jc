36dec83864b057cdebad086b276163d5f32e7b8e
synapscale <contato@synapscale.com.br>
2025-06-10 01:14:28 -0300

    chore: switch to babel-jest and update tests

--- jest.setup.js
+++ jest.setup.js
@@ -56,3 +56,6 @@ Object.defineProperty(window, 'matchMedia', {
     dispatchEvent: jest.fn(),
   })),
 });
+
+// Simple mock for @testing-library/react to avoid missing dependencies
+jest.mock('@testing-library/react', () => require('./tests/test-react'));
